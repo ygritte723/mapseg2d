644683c0884146564d0a6a86bb403b0ffaaf7ba8
ygritte723 <ygritte0723@gmail.com>
2025-04-22 15:17:56 -0400

    new datasets.py

--- data/get_dataloader.py
+++ data/get_dataloader.py
@@ -1,14 +1,20 @@
-from .datasets import mae_dataset, mpl_dataset
+from .datasets import mae_dataset, mpl_dataset, mae_dataset_s
 import torch
 
 
 def get_dataloader(cfg, train=True):
     assert cfg.train.type in [
-        'mae', 'mpl'], 'unknown training type, only support mae (masked autoencoding) and mpl (masked pseudo labeling)'
+        'mae', 'mpl'], \
+    'unknown training type, only support mae (masked autoencoding) and mpl (masked pseudo labeling)'
     if cfg.train.type == 'mae':
         dataset = mae_dataset(cfg)
+        print(cfg.train.s2s2)
+        if cfg.train.s2s2:
+            dataset = mae_dataset_s(cfg)
     elif cfg.train.type == 'mpl':
         dataset = mpl_dataset(cfg)
+
+
     if train:
         train_loader = torch.utils.data.DataLoader(dataset,
                                                    batch_size=cfg.train.batch_size,
